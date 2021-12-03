c717f31e4f34c1e8def72109f7a9e1e1c4c6c57d
Flavia Kosti <flaviakosti@gmail.com>
2021-12-03 23:14:57 +0100

    Create basic structure.

---
+++ basic-logic.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/reserve")
+def reserve():
+    return render_template("reserve.html")
+
+@app.route("/movie")
+def movie():
+    return render_template("movie.html")
+
+# @app.route("/path")
+# def test():
+# return redirect(url_for("movie", var=varMew))
+
+
+if __name__ == "__main__":
+    app.run()
